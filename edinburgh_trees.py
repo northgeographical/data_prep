6955cf39bdd63b7f330ed3b9e8bb828f414f4933
northgeographical <63863624+northgeographical@users.noreply.github.com>
2020-04-22 13:26:15 +0100

    Update edinburgh_trees.py

--- edinburgh_trees.py
+++ edinburgh_trees.py
@@ -54,6 +54,7 @@ outProj = Proj(init='epsg:4326')
 x1, y1 = eastings, northings
 x2, y2 = transform(inProj, outProj, x1, y1)
 
+# drop coordinates column
 data = data.drop(columns='Coordinates')
 
 # print all height ranges
